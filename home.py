4a4ec3282fdce26a596016318913f5665b5d3047
Daniel Gonzalez Osuna <dg8390@gmail.com>
2021-01-27 13:06:00 -0800

    Add files via upload

---
+++ home.py
@@ -0,0 +1,11 @@
+import time
+
+place = "home"
+action  = "miss"
+
+print (" I " + action + " my " + place)
+
+seconds = int(input("time remaining"))
+for i in range(seconds) :
+     print (str(seconds - i) + " seconds " )
+     time.sleep(1)
